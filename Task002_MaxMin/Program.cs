eb631a295a9530b1be3568eeb2c1fa6b434fc763
Akuloff.M <Akuloff.M@gmail.com>
2022-02-03 19:55:22 +0300

    add Task 002 Max-Min project

---
+++ Task002_MaxMin/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+
+Console.WriteLine(); // Пустая строка для отделения
+
+int max = numberA;
+int min = numberB;
+if(numberB > numberA) max = numberB;
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(max);
+}
+if(numberA < numberB) min = numberA;
+{
+    Console.Write("Меньшее число = ");
+    Console.WriteLine(min);
+}
